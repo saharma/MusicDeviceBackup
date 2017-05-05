c4d8c54ef8bdd6af942856379cfe57728456241a
saharma <saharmohdali@gmail.com>
2017-05-05 20:25:01 +0100

    Parses Tracks

--- src/main/java/mdbu/parser/libraryParser.java
+++ src/main/java/mdbu/parser/libraryParser.java
@@ -32,7 +32,6 @@ public class libraryParser {
 
         Collection<track> tracks = new ArrayList<track>();
 
-        track track = new track();
         //First dict children
         //+2 for just key nodes
 
@@ -54,22 +53,34 @@ public class libraryParser {
 
             if (libraryDataNode.getNodeValue().equals("Library Persistent ID")) {
            //     System.out.println(libraryDataNode.getNodeValue() + " : " + libraryDataValueNode.getNodeValue());
+
                 library_id = libraryDataValueNode.getNodeValue();
-                track.setLibrary_ID(library_id);
+
+
+
             }
 
             if (libraryDataNode.getNodeValue().equals("Tracks")) {
+
                 Integer track_id = null;
+                String name = null;
+                String artist = null;
+                String album = null;
+                String genre = null;
+                Integer size = null;
+                Integer year = null;
+
                 NodeList trackDicts = libraryDictKeys.item(i + 1).getChildNodes();
 
 
                 //loop for inner dict
                 // returns track key (track ID)
                 for (int j = 0; j < trackDicts.getLength(); j += 2) {
-                    tracks.add(track);
+
+
                     Node trackIDKey = trackDicts.item(j).getFirstChild();
                     track_id = Integer.valueOf(trackIDKey.getNodeValue());
-                    track.setTrack_id(track_id);
+
 
 
                     NodeList trackDataKeys = trackDicts.item(j + 1).getChildNodes();
@@ -79,37 +90,30 @@ public class libraryParser {
                         Node trackKey = trackDataKeys.item(k).getFirstChild();
                         Node trackValue = trackDataKeys.item(k + 1).getFirstChild();
 
-                        String name = null;
-                        String artist = null;
-                        String album = null;
-                        String genre = null;
-                        Integer size = null;
-                        Integer year = null;
+
 
                         if (trackKey.getNodeValue().equals("Name")) {
                             name = trackValue.getNodeValue();
-                            track.setName(name);
                         }
                         if (trackKey.getNodeValue().equals("Artist")) {
                             artist = trackValue.getNodeValue();
-                            track.setArtist(artist);
                         }
                         if (trackKey.getNodeValue().equals("Album")) {
                             album = trackValue.getNodeValue();
-                            track.setAlbum(album);
                         }
                         if (trackKey.getNodeValue().equals("Genre")) {
                             genre = trackValue.getNodeValue();
-                            track.setGenre(genre);
                         }
                         if (trackKey.getNodeValue().equals("Size")) {
                             size = Integer.valueOf(trackValue.getNodeValue());
-                            track.setSize(size);
                         }
                         if (trackKey.getNodeValue().equals("Year")) {
                             year = Integer.valueOf(trackValue.getNodeValue());
-                            track.setYear(year);
                         }
+
+
+
+
 
 
 
@@ -123,67 +127,19 @@ public class libraryParser {
 
                     }
 
+                    track newTrack = new track(track_id, name, artist, album, genre, year, size, library_id);
 
-                 //   System.out.println("");
-                }
-
-
-            }
-
-
-            if (libraryDataNode.getNodeValue().equals("Playlists")) {
-                NodeList playListDict = libraryDictKeys.item(i + 1).getChildNodes();
-
-
-                //plus one, no values nodes to follow
-                for (int l = 0; l < playListDict.getLength(); l += 1) {
-                    NodeList playListKeys = playListDict.item(l).getChildNodes();
-
-
-                    for (int m = 0; m < playListKeys.getLength(); m += 2) {
-                        NodeList playlistTracksDict = playListKeys.item(m + 1).getChildNodes();
-                        Node playListKey = playListKeys.item(m).getFirstChild();
-                        Node playlistValue = playListKeys.item(m + 1).getFirstChild();
-                        if (playListKey.getNodeValue().matches("Name|Playlist Persistent ID")) {
-
-                    //        System.out.println(playListKey.getNodeValue() + ":" + playlistValue.getNodeValue());
-
-
-                        } else if (playListKey.getNodeValue().equals("Playlist Items")) {
-                            //in playlistTrack Dict
-                            for (int n = 0; n < playlistTracksDict.getLength(); n += 1) {
-                                NodeList playlistTrackKeys = playlistTracksDict.item(n).getChildNodes();
-
-
-                                //playlistTrack keys
-                                for (int p = 0; p < playlistTrackKeys.getLength(); p += 2) {
-                                    //    Node playlistTrackKey = playlistTrackKeys.item(p).getFirstChild();
-                                    Node playlistTrackValue = playlistTrackKeys.item(p + 1).getFirstChild();
-                            //        System.out.println("Playlist Track: " + playlistTrackValue.getNodeValue());
-
-
-                                }
-
-                            }
-                        }
-                    }
-
-
-               //     System.out.println("");
-
+                    tracks.add(newTrack);
 
                 }
+
             }
 
         }
 
-
-
         for(track t: tracks){
             System.out.print(t);
             System.out.println("");}
-
-
 
     }
 
